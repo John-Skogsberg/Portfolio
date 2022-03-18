ec958c58ae7fa7ec58cee7fb0e7bad3fce24d12e
Dryden001 <51544458+Dryden001@users.noreply.github.com>
2022-03-18 00:52:22 -0600

    added engineering function

---
+++ CarouselID.js
@@ -0,0 +1,10 @@
+$('#carouselExampleIndicators').carousel({
+    interval: 4000,
+    wrap: true,
+    keyboard: true
+});
+$('#carouselExampleIndicators2').carousel({
+    interval: 6000,
+    wrap: true,
+    keyboard: true
+});
